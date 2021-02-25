583fb533ca92e52c9471b77de9abf4da17c5b5f7
Grant <nerfboyforever@gmail.com>
2021-02-25 03:11:20 -0600

    Version 0.7.0
    - Added localStorage functionality. Tabs and sub organization is now saved
    - Subs can now be moved to different tabs

--- content.js
+++ content.js
@@ -6,31 +6,17 @@ var innerGuide = document.getElementById("guide-inner-content")
 var widgetContainer = null
 var pulledSub = null
 var pulledMenu = null
-var subTabDict = {}
 var tabNodes = []
-var tabCount = 0
-
-
-
-// Get tabs (number of tabs, tab names, colors, etc)
-// Add the tabs that were found (if any)
-// Sort the subs into the tabs
-
-// When adding a tab, input its name
-// Tab gets created, sub gets added to that tab (and removed from any they are currently in)
-
-// If selecting a tab, sub gets added to that tab (and removed from any they are currently in)
-
-
-
-
+var subLinkDict = {}
+var subTabDict = {}
 
 
 function waitForPageLoad() {
     check = setInterval(function(){
+        if (document.getElementsByClassName("tab").length > 0) { return; }
         if (document.getElementById("sections")) {
             clearInterval(check);
-            console.log("Got Sections...")
+            console.log("Got Sections...");
             // Sections > Subscription Renderer > Subscription List
             // Get "Show # More" button (<a> element)
             // Click it
@@ -38,7 +24,6 @@ function waitForPageLoad() {
             expandBtn = subList.childNodes[subList.childNodes.length-1].childNodes[1].childNodes[1];
             expandBtn.click();
 
-            //subTabDict = getSubCookies()
             setupSubs();
         }
     }, 100);
@@ -46,10 +31,6 @@ function waitForPageLoad() {
 
 function setupSubs() {
     console.log("Youtube Sub Tabs")
-    // Get Second id=items div
-    // Set sub widgets to childnodes of that div
-    // Append the 'expandable' childnodes to widgets node list
-    // Remove 'expandable' div
 
     // Allow overflow to make subs visible
     guide.style.overflow = "visible"
@@ -68,6 +49,11 @@ function setupSubs() {
     expandableWidget = widgetContainer.childNodes[widgetContainer.childNodes.length-1]
     appendChildren(widgetContainer, Array.from(expandableWidget.childNodes[3].childNodes[1].childNodes))
     expandableWidget.remove()
+
+    // Get saved data
+    subLinkDict = JSON.parse(localStorage.getItem("subscription_links"));
+    if (!(subLinkDict)) { subLinkDict = {}; }
+    console.log(subLinkDict);
 
     // Get the sub widgets and add our new functionality to them
     // Add tabs to the container
@@ -93,15 +79,19 @@ function addSubSlides(nodes) {
         nodes[index].insertBefore(sub, nodes[index].firstChild);
 
         // If this subscription doesn't have a value in the subTab dictionary
-        if (!(sub.id in subTabDict)) {
-            tabNum = -1
-            subTabDict[sub.id] = tabNum;
+        if (!(sub.id in subLinkDict)) {
+            subLinkDict[sub.id] = -1;
+        } else {
+            tabNum = parseInt(subLinkDict[sub.id]);
             // Set sub as child of corresponding tab
-            if (tabNum >= 0) {
+            if (tabNum >= 0 && tabNodes[tabNum]) {
                 tabNodes[tabNum].appendChild(nodes[index]);
+            } else {
+                subLinkDict[sub.id] = -1;
             }
         }
     }
+    saveData();
 }
 
 function addSubListeners(nodes) {
@@ -128,7 +118,6 @@ function subMenu(e) {
     pulledMenu.style.top = difference.y + "px";
 
     pulledMenu.addEventListener('mouseout', closeMenu);
-    menuItem.addEventListener('click', createTab)
 }
 
 
@@ -147,6 +136,7 @@ function pullSub(e) {
 }
 
 function pushSub(e) {
+    e.stopPropagation()
     if (pulledMenu) { return }
     if (pulledSub) {
         pulledSub.firstChild.style.left = "0px"
@@ -154,72 +144,99 @@ function pushSub(e) {
         pulledSub.lastChild.style.backgroundColor = "white"
         pulledSub.removeEventListener('click', subMenu)
     }
-    e.stopPropagation()
 }
 
 function closeMenu(e) {
-    if (e.relatedTarget.parentElement == pulledMenu) { return } // JS stupidity causes 'mouseout' to call when hovering over child elements OF THE MENU!!!
+    if (e.relatedTarget.parentElement == pulledMenu || e.relatedTarget == pulledMenu) { return } // JS stupidity causes 'mouseout' to call when hovering over child elements OF THE MENU!!!
     pulledMenu.remove()
     pulledMenu = null;
     pushSub(e)
 }
 
 function createTab(e) {
+    e.stopPropagation();
     value = prompt("Enter tab name:");
     if (!(value)) { return; }
 
-    tab = document.createElement("div"); tab.className = "tab"; tab.id = value;
+    tab = document.createElement("div"); tab.className = "tab"; tab.title = value;
     widgetContainer.appendChild(tab);
     if (tabNodes.length>0) {
         insertAfter(tabNodes[tabNodes.length-1], tab);
     } else {
         widgetContainer.insertBefore(tab, widgetContainer.firstChild);
     }
+    tab.appendChild(pulledSub.parentElement);
+    subLinkDict[pulledSub.id] = parseInt(tabNodes.length); 
+
     tabNodes.push(tab)
+    subTabDict[tabNodes.length-1] = tab.title;
+    console.log(subTabDict);
+    saveData();
+}
+
+function moveSubToTab(e) {
+    e.stopPropagation();
+    console.log(e.target.id);
+    tabNodes[e.target.id].appendChild(pulledSub.parentElement);
+    subLinkDict[pulledSub.id] = parseInt(e.target.id);
+    saveData();
 }
 
 
 
-function getSubs(container) {
-    newList = [];
-    nodes = container.childNodes;
-    for (index in Array.from(nodes)) {
-        // If this child is a tab, work through its children and skip the tab itself
-        if (nodes[index].className == "tab") {
-            newList.push(getSubs(nodes[index].childNodes));
-            continue;
-        }
-        newList.push(nodes[index]);
-    }
-    return newList;
-}
 
 function setupTabMenu() {
     for (tab in tabNodes) {
-        menuItem = document.createElement("div"); menuItem.className = "menu-link"; menuItem.innerHTML = tabNodes[tab].id.toUpperCase();
+        menuItem = document.createElement("div"); menuItem.className = "menu-link"; menuItem.innerHTML = tabNodes[tab].title.toUpperCase(); menuItem.id = tab;
+        menuItem.addEventListener('click', moveSubToTab);
         pulledMenu.appendChild(menuItem);
     }
-
+    
     line = document.createElement("hr"); line.style = "border-top: 1px solid lightgray; width: 90%; margin: auto; padding-bottom: 5px; margin-top: 5px;";
     menuItem = document.createElement("div"); menuItem.className = "menu-link"; menuItem.innerHTML = "ADD A SUB";
+    menuItem.addEventListener('click', createTab);
     pulledMenu.appendChild(line); pulledMenu.appendChild(menuItem);
 }
 
 function setupTabs() {
-    // tabCount = localStorage.getItem("tabCount");
-    // Use JSON.stringify to encode lists to localStorage and decode them
-    if (!(tabCount)) {
-        tabCount = 0;
+    subTabDict = JSON.parse(localStorage.getItem("subscription_tabs"));
+    if (!(subTabDict)) {
+        subTabDict = {};
         return;
     }
-
-
-    for (x=0; x < 3; x++) {
-        tab = document.createElement("div"); tab.className = "tab";
-        container.appendChild(tab);
-        container.insertBefore(tab, container.firstChild);
+    
+    console.log(subTabDict);
+    for (key in subTabDict) {
+        tab = document.createElement("div"); tab.className = "tab"; tab.title = subTabDict[key];
+        widgetContainer.appendChild(tab);
+        widgetContainer.insertBefore(tab, widgetContainer.firstChild);
         tabNodes.push(tab)
     }
+}
+
+
+
+function saveData() {
+    localStorage.setItem("subscription_links", JSON.stringify(subLinkDict));
+    localStorage.setItem("subscription_tabs", JSON.stringify(subTabDict));
+}
+
+function getSubs(container) {
+    let newList = [];
+    let nodes = Array.from(container.childNodes);
+    // nodes.length = 0 if the container is empty, but not actually........... What the frick.
+    if (nodes.length < 1) { return null; }
+    for (index in nodes) {
+        // If this child is a tab, work through its children and skip the tab itself
+        if (nodes[index].className == "tab") {
+            add = getSubs(nodes[index]);
+            if (add) { newList.push(add); }
+            continue;
+        }
+        newList.push(nodes[index]);
+    }
+    console.log(newList);
+    return newList;
 }
 
 function getChannelID(node) {
